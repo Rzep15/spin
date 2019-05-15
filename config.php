b769dde42ef08684186a59f9b41af19364d9c842
Rzep15 <45102121+Rzep15@users.noreply.github.com>
2019-05-15 09:50:18 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "ekoprastio15@gmail.com";
+$password = "Ekokeren123";
+
+//Pillih Warna 0 untuk hijau 1 untuk kuning
+$collor = "1";
+
+// Pilih Jumlah Spin Saran 1 - 5
+$amount = "2";
+
+
+
